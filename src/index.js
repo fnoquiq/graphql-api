2248cbce2d4b521c84c3cd184f7a0ad8090837ab
fnoquiq <gabriel_t_ana@hotmail.com>
2019-08-17 10:47:10 -0300

    :construction: Definido estrutura inicial do projeto

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require("graphql-yoga");
+const Binding = require("prisma-binding");
+const { prisma } = require("./generated/prisma-client");
+
+const resolvers = require("./resolvers");
+const env = process.env;
+const endpoint = `${env.PRISMA_ENDPOINT}/${env.PRISMA_SERVICE}/${
+  env.PRISMA_STAGE
+}`;
+
+const server = new GraphQLServer({
+  typeDefs: `${__dirname}/schema.graphql`,
+  resolvers,
+  context: request => ({
+    ...request,
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: endpoint
+    }),
+    prisma
+  })
+});
+
+server
+  .start()
+  .then(() => console.log("Server running on http://localhost:4000..."));
