a49e386438d19c59c7f73faa134aa568f4c41511
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-08 17:44:34 -0300

    :hammer: Organiza vars de configuração

--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,14 @@
 const { GraphQLServer } = require("graphql-yoga");
-const Binding = require("prisma-binding");
-const { prisma } = require("./../../prisma/generated/prisma-client");
+const { PRISMA_CONFIG } = require("./../../config/default")
 
 const schema = require("./graphql/");
-
-const env = process.env;
-const endpoint = `${env.PRISMA_ENDPOINT}/${env.PRISMA_SERVICE}/${
-  env.PRISMA_STAGE
-}`;
 
 const server = new GraphQLServer({
   schema,
   context: request => ({
     ...request,
-    db: new Binding.Prisma({
-      typeDefs: `${__dirname}/../../prisma/generated/graphql-schema/prisma.graphql`,
-      endpoint: endpoint
-    }),
-    prisma
+    db: PRISMA_CONFIG.db,
+    prisma: PRISMA_CONFIG.prisma
   })
 });
 
