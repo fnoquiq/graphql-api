3174b04de4dac8030b1ae8afefd1fa1aa9604bad
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-17 00:30:08 -0300

    :hammer: Adiciona ignore lint para as variáveis info
    O lint não deixava passar um commit que possuia a var Info declarada sem ser usada, acontece que as vezes é impossivel fugir de não usar ela, por isso, foi adicionado uma tag para ignorar esse error no ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'info' }],
     'no-console': 0,
   },
 };
